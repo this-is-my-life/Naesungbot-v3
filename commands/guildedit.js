c6d057f82baf9f6abd24c69d24b33eed157933d4
pmh-only <pmhstudio.pmh@gmail.com>
2020-10-17 22:20:33 +0900

    Fix guildedit.js length problem

--- commands/guildedit.js
+++ commands/guildedit.js
@@ -6,8 +6,9 @@ module.exports.aliases = ['guildedit', 'ge']
  */
 module.exports.run = async (client, msg) => {
   if (!msg.member.hasPermission('MANAGE_GUILD')) return msg.reply('You have no permission!')
-  const [arg] = msg.query.args
+  const arg = msg.query.args.join(' ')
   if (!arg) return msg.reply(`${client.settings.prefix}guildedit guildname`)
+  if (arg.length > 100) return msg.reply('so long duh\nhttps://youtu.be/OCh2l0J1uJk?t=5')
   await msg.guild.edit({ name: arg })
 
   msg.reply('Changed guild name to `' + arg + '`')
